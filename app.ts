cb32743a3759aefdffdb33e0b98263877fce50b4
yoojin <yoojin@yoojinui-MacBookPro.local>
2022-08-03 14:38:05 +0900

    개발환경 세팅

---
+++ app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+const port = 3000;
+
+mongoose
+  .connect('mongodb://localhost:27017/daycarecenter')
+  .then(() => {
+    console.log('connected to mongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});

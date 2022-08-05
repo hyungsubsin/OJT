d964316070de6e976a7b0386296a1c3995e83002
yoojin <yoojin.rameau@gmail.com>
2022-08-05 14:14:18 +0900

    어린이집 스키마 설정

---
+++ schemas/childcarecenter.ts
@@ -0,0 +1,18 @@
+import { Schema, model, connect } from 'mongoose';
+import { Childcarecenter } from '../interfaces/Childcarecenter';
+
+const childcarecenterSchema = new Schema({
+  name: { type: String, required: true },
+  cellPhone: { type: String, required: true },
+  homePageUrl: String,
+  childrenCount: Number,
+  startAt: String,
+  use_naver_coord: { type: Boolean, required: true },
+  address: String,
+  lng: { type: Number, required: true },
+  lat: { type: Number, required: true },
+});
+
+const Childcarecenter = model<Childcarecenter>('Childcarecenter', childcarecenterSchema);
+
+export default Childcarecenter;

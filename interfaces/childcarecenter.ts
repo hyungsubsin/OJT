a7d3b5c5abd94ef37d9cb934ce366284df7cd3cf
yoojin <yoojin.rameau@gmail.com>
2022-08-05 14:40:29 +0900

    dto 작성

---
+++ interfaces/childcarecenter.ts
@@ -0,0 +1,23 @@
+export interface Childcarecenter {
+  name: string;
+  cellPhone: string;
+  homePageUrl: string;
+  childrenCount: number;
+  startAt: string;
+  use_naver_coord: boolean;
+  address: string;
+  lng: string;
+  lat: string;
+}
+
+export interface ChildcarecenterDTO {
+  name: string;
+  cellPhone: string;
+  homePageUrl: string;
+  childrenCount: number;
+  startAt: string;
+  use_naver_coord: boolean;
+  address: string;
+  lng: string;
+  lat: string;
+}

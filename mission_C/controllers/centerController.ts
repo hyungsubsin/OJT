3fc954e7fa896daf161ffc9037893dbca8152fff
yoojin <yoojin.rameau@gmail.com>
2022-08-05 14:45:47 +0900

    GET API 컨트롤러 작성

---
+++ mission_C/controllers/centerController.ts
@@ -0,0 +1,15 @@
+import { Router, Request, Response } from 'express';
+import centerService from '../services/centerService';
+import { ChildcarecenterDTO } from '../../interfaces/Childcarecenter';
+
+const getAllData = async (req: Request, res: Response) => {
+  try {
+    const data = await centerService.getChildCareCenter();
+    return res.json(data).status(200);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+};
+
+export default { getAllData };

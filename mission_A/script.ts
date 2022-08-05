734a4519dc64e0809a47fddae9207b54b52903c3
yoojin <yoojin.rameau@gmail.com>
2022-08-05 14:37:15 +0900

    어린이집 데이터 DB에 저장

--- mission_A/script.ts
+++ mission_A/script.ts
@@ -15,6 +15,28 @@ const getChildCareCenterData = async () => {
     if (nextVersionData.data.id) {
       version = nextVersionData.data.id;
     } // 업데이트가 있을 시 해당 버전으로 id 변경
+    const dataListURL = `${baseURL}/school/childcare/${version}`;
+    let dataList = await axios.get(dataListURL);
+    let centerList: any[] = [];
+    while (dataList.data.next) {
+      let cursor = dataList.data.next;
+      dataList = await axios.get(`${dataListURL}/?cursor=${cursor}&size=${size}`);
+      dataList.data.results.forEach((value: any) => {
+        centerList.push({
+          name: value.name,
+          cellPhone: value.cellPhone,
+          homePageUrl: value.homePageUrl,
+          childrenCount: value.childrenCount,
+          startAt: value.startAt,
+          use_naver_coord: value.use_naver_coord,
+          address: value.address,
+          lng: parseFloat(value.lng),
+          lat: parseFloat(value.lat),
+        });
+      });
+    }
+
+    await Childcarecenter.insertMany(centerList).then((error) => console.error(error));
   } catch (error) {
     console.error(error);
   }
